54faace99876ac0e545a7c65c8e4dfa11972e4a5
Daniel Kofler <fox27374@gmail.com>
2019-04-23 14:47:04 +0200

    initial

---
+++ dklib.py
@@ -0,0 +1,67 @@
+#!../env/bin/python
+
+import mysql.connector
+import csv
+import os
+
+def create_dbh():
+    dbh = mysql.connector.connect(
+        host="localhost",
+        user="avwattens",
+        passwd="QP?G3sFtE9@9ZaXY",
+        database="atwat01")
+
+    return dbh
+
+def query_mysql(id_event):
+    sql = get_sql_from_event(id_event)
+    cnx = create_dbh()
+    cursor = cnx.cursor()
+    cursor.execute(sql)
+    # Get header and rows
+    header = [i[0] for i in cursor.description]
+    rows = [list(i) for i in cursor.fetchall()]
+    # Append header to rows
+    rows.insert(0,header)
+    cursor.close()
+    cnx.close()
+    return rows
+
+def db_to_html(id_event):
+    data = query_mysql(id_event)
+    htable=u'<div class="table">'
+    # Create header
+    header = u'<div class="row header blue">'
+    for item in data[0]:
+        header += u'<div class="cell">' + unicode(item) + u'</div>'
+    header += u'</div>'
+    htable += header
+
+    del data[0]
+    for row in data:
+        newrow = u'<div class="row">'
+        newrow = newrow + ''.join([u'<div class="cell">' + unicode(x) + u'</div>' for x in row])
+        newrow += '</div>'
+        htable+= newrow
+    htable += '</div>'
+    return htable
+
+def db_to_csv(id_event):
+    csvfile = 'files/file{}.csv'.format(id_event)
+    data = query_mysql(id_event)
+    fp = open(csvfile, 'w')
+    myFile = csv.writer(fp)
+    myFile.writerows(data)
+    fp.close()
+
+def sql_html(query):
+	return nlist_to_html(query_mysql(query))
+
+def get_sql_from_event(id_event):
+    event_query_map={}
+    event_query_map['0001'] = "SELECT t_date as Datum, t_title as Anrede, t_name as Vorname, t_surname as Nachname, t_birth as Geburtstag, t_ebname as EBVorname, t_ebsurname as EBNachname, t_street as Strasse, t_number as Hausnummer, t_zip as PLZ, t_city as Stadt, t_section as Sektion, t_membernumber as AVNummer, t_guest as Gast, t_phone as Telefonnummer, t_mail as Email, t_membership as Jahreskarte, t_family as Familie FROM t0001 ORDER BY Datum desc;"
+
+    sql = event_query_map[id_event]
+
+    return sql
+
---
+++ index.py
@@ -0,0 +1,26 @@
+#!env/bin/python
+
+from cfg import dklib
+
+id_event = '0001'
+query = dklib.get_sql_from_event(id_event)
+
+# Create CSV
+dklib.db_to_csv(id_event)
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>AV Wattens</title>'
+print '<link rel="stylesheet" href="table.css">'
+print '</head>'
+print '<body>'
+print '<div class="wrapper">'
+print '<div class="navbar">'
+print '<a class="buttonDownload" href="files/file0001.csv" download="Jahreskarten.csv">Download CSV</a>'
+print '</div>'
+print dklib.db_to_html(id_event)
+print '</div>'
+print '</body>'
+print '</html>'
+
